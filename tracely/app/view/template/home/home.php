7f3b9e978ce6f4ba349cecbbbcea355fe486d22c
MalleMixeph <52078946+MalleMixeph@users.noreply.github.com>
2021-02-01 17:21:12 +0100

    Add files via upload

---
+++ tracely/app/view/template/home/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="static/style/style.css">
+  </head>
+  <body>
+    <h1>hallo</h1>
+    <a href="user/2">hier</a>
+    <img src="../../../" alt="">
+  </body>
+</html>
---
+++ tracely/app/view/template/user/detail.php
@@ -0,0 +1,5 @@
+<h1><?=$context['object']->getField('vorname')?> <?=$context['object']->getField('nachname')?></h1>
+
+<p>Email: <?=$context['object']->getField('email')?></p>
+<p>PLZ: <?=$context['object']->getField('plz')?></p>
+<p>Telefon: <?=$context['object']->getField('telefonnummer')?></p>
