e0f046cc2a860f66102bb5eb26e311e033f55bac
tlmnn-adrn <73664812+tlmnn-adrn@users.noreply.github.com>
2021-02-02 11:08:57 +0100

    Delete tracely directory

--- tracely/core/base/model/sql_error.php
+++
@@ -1,6 +0,0 @@
-<h1>Es Gab einen SQL-Fehler:</h1>
-
-<br>
-
-<?php
-    echo $statement->errorInfo()[2];
--- tracely/core/base/model/to_many_objects_error.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    function throw_to_many_objects(){
-
-?>
-        <h1>Mehr als ein Objekt entspricht dem Filter. Verwende einen anderen Filter oder die filteredList Methode!</h1>
-<?php
-
-    }
