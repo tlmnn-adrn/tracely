7185b4635b27227adb3707d504b82961f931be64
tlmnn-adrn <73664812+tlmnn-adrn@users.noreply.github.com>
2021-02-02 13:36:04 +0100

    Delete tracely directory

--- tracely/core/base/model/fields/templates/email.php
+++
@@ -1,6 +0,0 @@
-<label for="<?=$name?>Field"><?=$label?></label>
-<input type="email" id="<?=$name?>Field" name="<?=$name?>" placeholder="<?=$placeholder?>" value="<?=$this->value?>" class="<?=$class?>">
-
-<?php foreach($this->errors as $error): ?>
-    <?=$error?><br>
-<?php endforeach; ?>
